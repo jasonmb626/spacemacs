4c187da1c96bd636d437db4423bbcbff3e9e0129
Jason Brunelle <jasonmb626@gmail.com>
2024-09-30 07:33:44 -0500

    Add symlink to templates

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -2,3 +2,5 @@
 
 ln -s ~/git/spacemacs ~/.emacs.d
 ln -s ~/git/spacemacs/.spacemacs ~/.spacemacs
+mkdir -p ~/Documents/zettelkasten
+ln -s ~/git/spacemacs/templates ~/Documents/zettelkasten/templates
